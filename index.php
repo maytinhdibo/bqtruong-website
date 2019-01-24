59c1cfd65dac15e203541949274cad9258e5b3fa
bqcuong <bqcuong2212@gmail.com>
2019-01-24 12:46:21 +0000

    add messenger chat

--- index.php
+++ index.php
@@ -41,24 +41,36 @@
 </head>
 
 <body>
+    <!-- Load Facebook SDK for JavaScript -->
+    <div class="fb-customerchat"
+         page_id="1669656373306437"
+         logged_in_greeting="Xin chào! mình có thể hỗ trợ bạn điều gì?"
+         logged_out_greeting="Xin chào! mình có thể hỗ trợ bạn điều gì?"
+    >
+    </div>
 
+    <script>
+        window.fbAsyncInit = function () {
+            FB.init({
+                appId            : '295027501369343',
+                autoLogAppEvents : true,
+                xfbml            : true,
+                version          : 'v3.2'
+            });
 
-        <!-- Load Facebook SDK for JavaScript -->
-<div id="fb-root"></div>
-<script>(function(d, s, id) {
-  var js, fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) return;
-  js = d.createElement(s); js.id = id;
-  js.src = 'https://connect.facebook.net/vi_VN/sdk/xfbml.customerchat.js';
-  fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));</script>
-
-<!-- Your customer chat code -->
-<div class="fb-customerchat"
-  attribution=setup_tool
-  page_id="1053231291479293">
-</div>
-
+            // FB.CustomerChat.show();
+        };
+        (function (d, s, id) {
+            var js, fjs = d.getElementsByTagName(s)[0];
+            if (d.getElementById(id)) {
+                return;
+            }
+            js = d.createElement(s);
+            js.id = id;
+            js.src = "https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js";
+            fjs.parentNode.insertBefore(js, fjs);
+        }(document, 'script', 'facebook-jssdk'));
+    </script>
 
     <nav class="navbar navbar-expand-lg fixed-top ">
         <a class="navbar-brand mr-4" href="#">Bùi Quốc Trưởng</a>
