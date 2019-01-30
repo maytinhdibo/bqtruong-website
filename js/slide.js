cdd8d482f9922cc05c8df6eb0874aa9292b8930c
maytinhdibo <iammaytinhdibo@gmail.com>
2019-01-30 16:25:01 +0700

    fix feedback card js

--- js/slide.js
+++ js/slide.js
@@ -1,74 +1,80 @@
 $('.talk-aboutme').slick({
-    infinite: true,
-    slidesToShow: 2,
-    slidesToScroll: 1,
-    arrows: true,
-    // autoplay: true,
-    autoplaySpeed: 2000,
-    dots:true,
-    edge: 'slick',
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 1
-        }
+  infinite: true,
+  slidesToShow: 2,
+  slidesToScroll: 1,
+  arrows: true,
+  // autoplay: true,
+  autoplaySpeed: 2000,
+  dots: true,
+  edge: 'slick',
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 1
       }
-    ]
-  });
+    }
+  ]
+});
 
-  $('#list-project').slick({
-    infinite: true,
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    arrows: true,
-    autoplay: true,
-    autoplaySpeed: 2000,
-    edge: 'slick',
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 2
-        }
-      },
-      {
-        breakpoint: 786,
-        settings: {
-          slidesToShow: 1
-        }
+$('#list-project').slick({
+  infinite: true,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  arrows: true,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  edge: 'slick',
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 2
       }
-    ]
-  });
-
-  $('#friend').slick({
-    infinite: true,
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    arrows: true,
-    autoplay: true,
-    autoplaySpeed: 2000,
-    edge: 'slick',
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 2
-        }
-      },
-      {
-        breakpoint: 786,
-        settings: {
-          slidesToShow: 1
-        }
+    },
+    {
+      breakpoint: 786,
+      settings: {
+        slidesToShow: 1
       }
-    ]
-  });
+    }
+  ]
+});
 
-  //when load end
-  var maxHeight=0;
-  $(".client-content").each(function(){
+$('#friend').slick({
+  infinite: true,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  arrows: true,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  edge: 'slick',
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 2
+      }
+    },
+    {
+      breakpoint: 786,
+      settings: {
+        slidesToShow: 1
+      }
+    }
+  ]
+});
+
+//when resize
+function feedbackCard() {
+  var maxHeight = 0;
+  $(".client-content").each(function () {
     if ($(this).height() > maxHeight) { maxHeight = $(this).height(); }
- });
- 
- $(".client-content").height(maxHeight);
+  });
+
+  $(".client-content").height(maxHeight);
+}
+feedbackCard();
+$(window).resize(function () {
+  feedbackCard();
+});
